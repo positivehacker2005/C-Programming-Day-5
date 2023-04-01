043f9020b66e4013836eb34c9622e895dd119914
Ramachandraa PS <62879218+positivehacker2005@users.noreply.github.com>
2023-04-01 13:42:59 +0530

    Add files via upload

---
+++ 12.C program to print rectangle symbol pattern.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char symbol;
+    int width, height, i, j;
+
+    printf("Enter a symbol to use: ");
+    scanf("%c", &symbol);
+
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+
+    printf("Enter the height of the rectangle: ");
+    scanf("%d", &height);
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            printf("%c ", symbol);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
